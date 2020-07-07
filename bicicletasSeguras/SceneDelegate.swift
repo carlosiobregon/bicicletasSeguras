b3df9ab4ea00caefaa63abe0309876bb3b090261
ciobregon <ciobregon@copaair.com>
2020-07-07 00:32:49 -0500

    Base project

--- bicicletasSeguras/SceneDelegate.swift
+++ bicicletasSeguras/SceneDelegate.swift
@@ -2,7 +2,7 @@
 //  SceneDelegate.swift
 //  bicicletasSeguras
 //
-//  Created by Carlos Obregón on 6/23/20.
+//  Created by Carlos Obregón on 7/6/20.
 //  Copyright © 2020 Carlos Obregón. All rights reserved.
 //
 
@@ -13,7 +13,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    /*func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
@@ -46,7 +46,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
-    }
+    }*/
 
 
 }
